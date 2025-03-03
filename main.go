d6db184dde214627f5b03193a7ab9d5762bfd266
Giusier <giusier@hotmail.com>
2025-03-03 13:14:06 -0300

    added /ping and /events routes

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/ping", ping)
+	server.GET("/events", getEvents)
+
+	server.Run(":8080")
+}
+
+func ping(context *gin.Context) {
+	context.JSON(http.StatusOK, "pong!")
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Hello world!"})
 }
